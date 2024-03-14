61d99c4425940d17cdcba12054c4337337f4c9cf
Nicolas Rühling <nicolas.ruehling@uni-potsdam.de>
2024-03-14 10:57:44 -0300

    uses importlib.resources and added __init__.py to encodings folder

--- src/coomsolver/utils/__init__.py
+++ src/coomsolver/utils/__init__.py
@@ -2,10 +2,7 @@
 Utilities.
 """
 
-# import atexit
-# from contextlib import ExitStack
-
-from importlib_resources import as_file, files
+from importlib.resources import as_file, files
 
 
 def get_file_path(package: str, file_name: str) -> str:
@@ -18,10 +15,5 @@ def get_file_path(package: str, file_name: str) -> str:
     Returns:
         str: The string for the path to the file
     """
-    # file_manager = ExitStack()
-    # atexit.register(file_manager.close)
-    # ref = files(package)
-    # file = file_manager.enter_context(as_file(ref / file_name))
-    # return str(file)
     with as_file(files(package) / file_name) as file:
         return str(file)
