b774d1c2f1e756cff12cf5720a95e61e098ff70c
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-02-05 20:14:25 +0100

    refactor __main__

--- src/coomsuite/__main__.py
+++ src/coomsuite/__main__.py
@@ -3,9 +3,10 @@ The main entry point for the application.
 """
 
 import sys
+from os.path import basename, join, splitext
 from tempfile import TemporaryDirectory
 
-from . import convert_instance, solve
+from . import convert_coom, solve, write_facts
 from .bounds.solver import BoundSolver
 from .preprocess import preprocess
 from .utils.logging import configure_logging, get_logger
@@ -27,38 +28,39 @@ def main() -> None:
     # log.debug("debug")
     # log.error("error")
 
+    log.info("Converting COOM file %s", args.input)
+    serialized_facts, unbounded = convert_coom(args.input, coom_user=args.user_input if args.user_input else None)
+
     if args.command == "convert":
-        asp_instance = convert_instance(args.input, "model", args.output)
+        # asp_instance = convert_instance(args.input, "model", args.output)
         if args.output is None:
-            print(asp_instance)
-
-        if args.user_input:
-            output_user_lp_file = convert_instance(args.user_input, "user", args.output)
-
-            if args.output is None:
-                print("")
-                print(output_user_lp_file)
+            print(serialized_facts)
+        else:
+            write_facts(serialized_facts, join(args.output, splitext(basename(args.input))[0] + "-coom.lp"))
 
     elif args.command == "solve":
-        log.info("Converting and solving COOM file %s", args.input)
+        log.info("Solving COOM file %s", args.input)
 
         with TemporaryDirectory() as temp_dir:
-            # Parse COOM to ASP serialized facts
-            serialized_facts = [convert_instance(args.input, "model", temp_dir)] + (
-                [convert_instance(args.user_input, "user", temp_dir)] if args.user_input else []
-            )
+            # # Parse COOM to ASP serialized facts
+            # serialized_facts = [convert_instance(args.input, "model", temp_dir)] + (
+            #     [convert_instance(args.user_input, "user", temp_dir)] if args.user_input else []
+            # )
+            temp_file = join(temp_dir, "serialized-facts.lp")
+            log.info("Saving serialized facts to %s", temp_file)
+            write_facts(serialized_facts, temp_file)
 
             if args.show_facts:
-                print("\n".join(preprocess(serialized_facts, discrete=True)))  # nocoverage
-            elif args.bounds:
-                bound_solver = BoundSolver(serialized_facts, args.solver, solver_args, args.output)
+                print("\n".join(preprocess([temp_file], discrete=True)))  # nocoverage
+            elif unbounded:
+                bound_solver = BoundSolver([temp_file], args.solver, solver_args, args.output)
                 bound = bound_solver.get_bounds(
                     algorithm=args.bounds, initial_bound=args.initial_bound, use_multishot=args.multishot
                 )
 
                 print(f"\n The minimal upper bound is {bound}")
             else:
-                solve(serialized_facts, args.solver, 99, solver_args, args.output)
+                solve([temp_file], args.solver, 0, solver_args, args.output)
 
 
 if __name__ == "__main__":
--- tests/test_user_input.py
+++ tests/test_user_input.py
@@ -37,19 +37,19 @@ class TestUserInputCheck(TestCase):
         """
         Test user input check
         """
-        self.user_check("set_invalid_variable", "Invalid user input.\nVariable root.color[0] does not exist.")
-        self.user_check("add_invalid_variable", "Invalid user input.\nVariable root.basket[0] does not exist.")
+        self.user_check("set_invalid_variable", 'Invalid user input.\nVariable "root.color[0]" does not exist.')
+        self.user_check("add_invalid_variable", 'Invalid user input.\nVariable "root.basket[0]" does not exist.')
         self.user_check(
             "set_invalid_type",
-            "Invalid user input.\nNo value can be set for variable root.basket[0]. Variable exists but is a part.",
+            'Invalid user input.\nNo value can be set for variable "root.basket[0]". Variable exists but is a part.',
         )
         # self.user_check(
         #     "add_invalid_type", "Invalid user input.\nVariable root.basket[0] cannot be added: Not a part."
         # )
         self.user_check(
             "set_invalid_value_discrete",
-            "Invalid user input.\nValue 'Yellow' is not in domain of variable root.color[0].",
+            'Invalid user input.\nValue "Yellow" is not in domain of variable "root.color[0]".',
         )
         self.user_check(
-            "set_invalid_value_num", "Invalid user input.\nValue '11' is not in domain of variable root.size[0]."
+            "set_invalid_value_num", 'Invalid user input.\nValue "11" is not in domain of variable "root.size[0]".'
         )
