0fe6f5586d4cebe4c5c97b83f149b945ef81fd83
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-03-09 13:14:16 +0100

    initial bound is used for --show-facts option

--- src/coomsuite/__main__.py
+++ src/coomsuite/__main__.py
@@ -48,7 +48,7 @@ def main() -> None:
             write_facts(serialized_facts, temp_file)
 
             if args.show_facts:
-                print("\n".join(preprocess([temp_file], discrete=True)))  # nocoverage
+                print("\n".join(preprocess([temp_file], max_bound=args.initial_bound, discrete=True)))  # nocoverage
             elif unbounded:
                 bound_solver = BoundSolver([temp_file], args.solver, solver_args + ["--opt-mode=ignore"], args.output)
                 bound = bound_solver.get_bounds(
