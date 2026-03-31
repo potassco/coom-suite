88015484228a70a804f28c3b45026130247ae869
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-03-31 15:15:47 +0200

    copied over general changes from association branch

--- tests/clintests/tests_solve.py
+++ tests/clintests/tests_solve.py
@@ -1,6 +1,6 @@
 """
 Contains a dictionary with all clintest tests for solving
-and the corresponding files or programs they should be run with.
+and the corresponding file (usually corresponds to "testname.lp") or programs they should be run with.
 
 The key of the dictionary corresponds to the name of the test.
 
@@ -8,6 +8,7 @@ Some tests contain a "ftest" entry which is a test modified especially to work w
 All other tests work with both clingo and flingo.
 """
 
+# pylint: disable=line-too-long, too-many-lines
 from typing import Any
 
 from . import TEST_EMPTY, TEST_UNSAT, OptimalModel, StableModels
@@ -26,23 +27,18 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
     },
     "mandatory_part": {
         "test": StableModels({'include("root.a[0]")'}),
-        "files": ["mandatory_part.lp"],
     },
     "part_with_cardinality": {
         "test": StableModels({'include("root.a[0]")'}, {'include("root.a[0]")', 'include("root.a[1]")'}),
-        "files": ["part_with_cardinality.lp"],
     },
     "optional_part_with_subpart": {
         "test": StableModels(set(), {'include("root.a[0]")', 'include("root.a[0].b[0]")'}),
-        "files": ["optional_part_with_subpart.lp"],
     },
     "simple_discrete": {
         "test": StableModels({'value("root.a[0]","A1")'}, {'value("root.a[0]","A2")'}),
-        "files": ["simple_discrete.lp"],
     },
     "optional_discrete": {
         "test": StableModels(set(), {'value("root.a[0]","A1")'}, {'value("root.a[0]","A2")'}),
-        "files": ["optional_discrete.lp"],
     },
     "multiple_discrete": {
         "test": StableModels(
@@ -51,15 +47,12 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             {'value("root.a[0]","A2")', 'value("root.a[1]","A1")'},
             {'value("root.a[0]","A2")', 'value("root.a[1]","A2")'},
         ),
-        "files": ["multiple_discrete.lp"],
     },
     "simple_integer": {
         "test": StableModels({'value("root.a[0]",1)'}, {'value("root.a[0]",2)'}),
-        "files": ["simple_integer.lp"],
     },
     "optional_integer": {
         "test": StableModels(set(), {'value("root.a[0]",1)'}, {'value("root.a[0]",2)'}),
-        "files": ["optional_integer.lp"],
     },
     "multiple_integer": {
         "test": StableModels(
@@ -68,19 +61,15 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             {'value("root.a[0]",2)', 'value("root.a[1]",1)'},
             {'value("root.a[0]",2)', 'value("root.a[1]",2)'},
         ),
-        "files": ["multiple_integer.lp"],
     },
     "unbounded_integer": {
         "test": StableModels({'value("root.a[0]",1)'}, {'value("root.a[0]",2)'}),
-        "files": ["unbounded_integer.lp"],
     },
     "unbounded_integer_below": {
         "test": StableModels({'value("root.a[0]",1)'}, {'value("root.a[0]",2)'}),
-        "files": ["unbounded_integer_below.lp"],
     },
     "unbounded_integer_above": {
         "test": StableModels({'value("root.a[0]",1)'}, {'value("root.a[0]",2)'}),
-        "files": ["unbounded_integer_above.lp"],
     },
     "eq_sat": {
         "test": TEST_EMPTY,
@@ -268,7 +257,6 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             {'value("root.x[0]","A2")', 'value("root.y[0]","A1")'},
             {'value("root.x[0]","A3")', 'value("root.y[0]","A2")'},
         ),
-        "files": ["table_discrete.lp"],
     },
     "table_integer": {
         "test": StableModels(
@@ -277,7 +265,6 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             {'value("root.x[0]",2)', 'value("root.y[0]",1)'},
             {'value("root.x[0]",3)', 'value("root.y[0]",2)'},
         ),
-        "files": ["table_integer.lp"],
     },
     "table_mixed": {
         "test": StableModels(
@@ -286,7 +273,6 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             {'value("root.x[0]","A2")', 'value("root.y[0]",1)'},
             {'value("root.x[0]","A3")', 'value("root.y[0]",2)'},
         ),
-        "files": ["table_mixed.lp"],
     },
     "table_wildcard": {
         "test": StableModels(
@@ -294,11 +280,9 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             {'value("root.x[0]","A2")', 'value("root.y[0]","A1")'},
             {'value("root.x[0]","A2")', 'value("root.y[0]","A2")'},
         ),
-        "files": ["table_wildcard.lp"],
     },
     "table_undef": {
         "test": StableModels({'value("root.x[0]","A1")'}, {'value("root.x[0]","A2")'}),
-        "files": ["table_undef.lp"],
     },
     "table_undef2": {
         "test": TEST_EMPTY,
@@ -310,7 +294,6 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
     },
     "empty_table": {
         "test": TEST_UNSAT,
-        "files": ["empty_table.lp"],
     },
     "plus_sat": {
         "test": TEST_EMPTY,
@@ -472,13 +455,11 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
     },
     "count": {
         "test": StableModels({'include("root.x[0]")', 'include("root.x[1]")'}),
-        "files": ["count.lp"],
     },
     "sum": {
         "test": StableModels(
             {'value("root.x[0]",1)', 'value("root.x[1]",2)'}, {'value("root.x[0]",2)', 'value("root.x[1]",1)'}
         ),
-        "files": ["sum.lp"],
     },
     "min": {
         "test": StableModels(
@@ -486,45 +467,36 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             {'value("root.x[0]",3)', 'value("root.x[1]",3)'},
             {'value("root.x[0]",3)', 'value("root.x[1]",4)'},
         ),
-        "files": ["min.lp"],
     },
     "max": {
         "test": StableModels({'value("root.x[0]",3)', 'value("root.x[1]",3)'}),
-        "files": ["max.lp"],
     },
-    "simple_default": {"test": StableModels({'value("root.color[0]","White")'}), "files": ["simple_default.lp"]},
+    "simple_default": {"test": StableModels({'value("root.color[0]","White")'})},
     "simple_default_user": {
         "test": StableModels({'value("root.color[0]","Black")'}),
-        "files": ["simple_default_user.lp"],
     },
     "simple_default_include": {
         "test": StableModels(set(), {'include("root.bike[0]")', 'value("root.bike[0].color[0]","White")'}),
-        "files": ["simple_default_include.lp"],
     },
     "minimize": {
         "test": OptimalModel({'value("root.totalWeight[0]",1)'}),
         "ftest": OptimalModel({'value("root.totalWeight[0]",1)'}, flingo=True),
-        "files": ["minimize.lp"],
     },
     "maximize": {
         "test": OptimalModel({'value("root.totalOutput[0]",10)'}),
         "ftest": OptimalModel({'value("root.totalOutput[0]",10)'}, flingo=True),
-        "files": ["maximize.lp"],
     },
     "minimize_priority": {
         "test": OptimalModel({'include("root.bags[0]")', 'value("root.bags[0].volume[0]",0) '}),
         "ftest": OptimalModel({'include("root.bags[0]")', 'value("root.bags[0].volume[0]",0) '}, flingo=True),
-        "files": ["minimize_priority.lp"],
     },
     "maximize_priority": {
         "test": OptimalModel({'include("root.bags[0]")', 'value("root.bags[0].volume[0]",10)'}),
         "ftest": OptimalModel({'include("root.bags[0]")', 'value("root.bags[0].volume[0]",10) '}, flingo=True),
-        "files": ["maximize_priority.lp"],
     },
     "minimize_maximize_function": {
         "test": OptimalModel({'include("root.bags[0]")', 'value("root.bags[0].volume[0]",10)'}),
         "ftest": OptimalModel({'include("root.bags[0]")', 'value("root.bags[0].volume[0]",10)'}, flingo=True),
-        "files": ["minimize_maximize_function.lp"],
     },
     "maximize_minimize_function": {
         "test": OptimalModel(
@@ -544,15 +516,17 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
             },
             flingo=True,
         ),
-        "files": ["maximize_minimize_function.lp"],
     },
-    "add_part": {"test": StableModels({'include("root.a[0]")'}), "files": ["add_part.lp"]},
+    "add_part": {"test": StableModels({'include("root.a[0]")'})},
     "add_attribute": {
         "test": StableModels({'value("root.basket[0]","White")'}, {'value("root.basket[0]","Black")'}),
-        "files": ["add_attribute.lp"],
     },
-    "set_value_discrete": {"test": StableModels({'value("root.a[0]","A1")'}), "files": ["set_value_discrete.lp"]},
-    "set_value_integer": {"test": StableModels({'value("root.a[0]",1)'}), "files": ["set_value_integer.lp"]},
+    "set_value_discrete": {
+        "test": StableModels({'value("root.a[0]","A1")'}),
+    },
+    "set_value_integer": {
+        "test": StableModels({'value("root.a[0]",1)'}),
+    },
     "add_invalid_variable": {
         "test": StableModels(set()),
         "program": """
@@ -561,14 +535,11 @@ TESTS_SOLVE: dict[str, dict[str, Any]] = {
     "set_invalid_variable": {"test": StableModels(set()), "program": """user_value("root.color[0]","Yellow")."""},
     "set_invalid_type": {
         "test": StableModels(set(), {'include("root.basket[0]")'}),
-        "files": ["set_invalid_type.lp"],
     },
     "set_invalid_value_discrete": {
         "test": StableModels({'value("root.color[0]","Red")'}),
-        "files": ["set_invalid_value_discrete.lp"],
     },
     "set_invalid_value_num": {
         "test": StableModels({'value("root.size[0]",1)'}, {'value("root.size[0]",2)'}, {'value("root.size[0]",3)'}),
-        "files": ["set_invalid_value_num.lp"],
     },
 }
